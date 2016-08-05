811c6fece7985026dafd1b760717aa4a9300cfcc
Dmitry Malinovsky <damalinov@gmail.com>
2016-08-05 12:56:52 +0600

    Fix various errors in types.py

--- amqpframe/types.py
+++ amqpframe/types.py
@@ -6,6 +6,7 @@ import itertools
 
 from . import errors
 
+
 def grouper(iterable, n, fillvalue=None):
     """Collect data into fixed-length chunks or blocks"""
     # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
@@ -25,25 +26,26 @@ class BaseType:
     TABLE_LABEL = None
 
     def to_bytestream(self, stream: io.BytesIO):
-        try:
-            stream.write(self.pack(self))
-        except struct.error:
-            raise errors.InternalError(
-                'Unable to pack {}'.format(self)
-            )
+         stream.write(self.pack())
+#        try:
+#            stream.write(self.pack())
+#        except struct.error as exc:
+#            raise errors.InternalError(
+#                'Unable to pack {}'.format(self)
+#            ) from exc
 
     @classmethod
     def from_bytestream(cls, stream: io.BytesIO):
-        try:
-            x, = cls.unpack(stream)
-        except struct.error:
-            raise errors.SyntaxError(
-                'Unable to unpack {} from the stream'.format(cls.__name__)
-            )
+        x, _ = cls.unpack(stream)
+        #try:
+        #    x, _ = cls.unpack(stream)
+        #except struct.error as exc:
+        #    raise errors.SyntaxError(
+        #        'Unable to unpack {} from the stream'.format(cls.__name__)
+        #    ) from exc
         return cls(x)
 
-    @classmethod
-    def pack(cls, value):
+    def pack(self):
         raise NotImplementedError
 
     @classmethod
@@ -69,7 +71,9 @@ class Bool(BaseType):
 
     TABLE_LABEL = b't'
 
-    def __init__(self, value):
+    def __init__(self, value=False):
+        if isinstance(value, Bool):
+            value = value.value
         self.value = value
 
     def __eq__(self, other):
@@ -86,7 +90,7 @@ class Bool(BaseType):
     @classmethod
     def from_bytestream(cls, stream: io.BytesIO):
         try:
-            x, = cls.unpack(stream)
+            x, _ = cls.unpack(stream)
         except struct.error:
             raise errors.SyntaxError(
                 'Unable to unpack {} from the stream'.format(cls.__name__)
@@ -95,14 +99,14 @@ class Bool(BaseType):
 
     @classmethod
     def unpack(cls, stream):
-        return struct.unpack('!?', stream.read(1))
+        return struct.unpack('!?', stream.read(1))[0], 1
 
     @classmethod
     def pack_many(cls, value):
         result = []
         for values in grouper(value, 8, False):
             total = ''.join('1' if v else '0' for v in values)
-            result.append(UnsignedByte.pack(int(total, 2)))
+            result.append(UnsignedByte(int(total, 2)).pack())
         return b''.join(result)
 
     @classmethod
@@ -120,6 +124,9 @@ class Bool(BaseType):
     def many_from_bytestream(cls, stream, number_of_bits):
         return cls.unpack_many(stream, number_of_bits)
 
+    def __repr__(self):
+        return '<{}: {} at {}>'.format(self.__class__.__name__, bool(self), id(self))
+
 Bit = Bool
 
 
@@ -128,7 +135,7 @@ class _Bounded:
     MIN = None
     MAX = None
 
-    def __new__(cls, value):
+    def __new__(cls, value=0):
         assert cls.MIN <= value <= cls.MAX
         return super().__new__(cls, value)
 
@@ -139,13 +146,12 @@ class SignedByte(BaseType, _Bounded, int):
     MIN = -(1 << 7)
     MAX = (1 << 7) - 1
 
-    @classmethod
-    def pack(cls, value):
-        return struct.pack('!b', value)
+    def pack(self):
+        return struct.pack('!b', self)
 
     @classmethod
     def unpack(cls, stream: io.BytesIO):
-        return struct.unpack('!b', stream.read(1))
+        return struct.unpack('!b', stream.read(1))[0], 1
 
 
 class UnsignedByte(BaseType, _Bounded, int):
@@ -154,13 +160,12 @@ class UnsignedByte(BaseType, _Bounded, int):
     MIN = 0
     MAX = (1 << 8) - 1
 
-    @classmethod
-    def pack(cls, value):
-        return struct.pack('!B', value)
+    def pack(self):
+        return struct.pack('!B', self)
 
     @classmethod
     def unpack(cls, stream: io.BytesIO):
-        return struct.unpack('!B', stream.read(1))
+        return struct.unpack('!B', stream.read(1))[0], 1
 
 Octet = UnsignedByte
 
@@ -171,13 +176,12 @@ class SignedShort(BaseType, _Bounded, int):
     MIN = -(1 << 15)
     MAX = (1 << 15) - 1
 
-    @classmethod
-    def pack(cls, value):
-        return struct.pack('!h', value)
+    def pack(self):
+        return struct.pack('!h', self)
 
     @classmethod
     def unpack(cls, stream: io.BytesIO):
-        return struct.unpack('!h', stream.read(2))
+        return struct.unpack('!h', stream.read(2))[0], 2
 
 
 class UnsignedShort(BaseType, _Bounded, int):
@@ -186,13 +190,12 @@ class UnsignedShort(BaseType, _Bounded, int):
     MIN = 0
     MAX = (1 << 16) - 1
 
-    @classmethod
-    def pack(cls, value):
-        return struct.pack('!H', value)
+    def pack(self):
+        return struct.pack('!H', self)
 
     @classmethod
     def unpack(cls, stream: io.BytesIO):
-        return struct.unpack('!H', stream.read(2))
+        return struct.unpack('!H', stream.read(2))[0], 2
 
 Short = UnsignedShort
 
@@ -203,13 +206,12 @@ class SignedLong(BaseType, _Bounded, int):
     MIN = -(1 << 31)
     MAX = (1 << 31) - 1
 
-    @classmethod
-    def pack(cls, value):
-        return struct.pack('!l', value)
+    def pack(self):
+        return struct.pack('!l', self)
 
     @classmethod
     def unpack(cls, stream: io.BytesIO):
-        return struct.unpack('!l', stream.read(4))
+        return struct.unpack('!l', stream.read(4))[0], 4
 
 
 class UnsignedLong(BaseType, _Bounded, int):
@@ -218,13 +220,12 @@ class UnsignedLong(BaseType, _Bounded, int):
     MIN = 0
     MAX = (1 << 32) - 1
 
-    @classmethod
-    def pack(cls, value):
-        return struct.pack('!L', value)
+    def pack(self):
+        return struct.pack('!L', self)
 
     @classmethod
     def unpack(cls, stream: io.BytesIO):
-        return struct.unpack('!L', stream.read(4))
+        return struct.unpack('!L', stream.read(4))[0], 4
 
 Long = UnsignedLong
 
@@ -235,29 +236,28 @@ class SignedLongLong(BaseType, _Bounded, int):
     MIN = -(1 << 63)
     MAX = (1 << 63) - 1
 
-    @classmethod
-    def pack(cls, value):
-        return struct.pack('!q', value)
+    def pack(self):
+        return struct.pack('!q', self)
 
     @classmethod
     def unpack(cls, stream: io.BytesIO):
-        return struct.unpack('!q', stream.read(8))
+        return struct.unpack('!q', stream.read(8))[0], 8
 
 
 class UnsignedLongLong(BaseType, _Bounded, int):
     # Missing in rabbitmq/qpid
-    TABLE_LABEL = None
+    # Or not?
+    TABLE_LABEL = b'L'
 
     MIN = 0
     MAX = (1 << 64) - 1
 
-    @classmethod
-    def pack(cls, value):
-        return struct.pack('!Q', value)
+    def pack(self):
+        return struct.pack('!Q', self)
 
     @classmethod
     def unpack(cls, stream: io.BytesIO):
-        return struct.unpack('!Q', stream.read(8))
+        return struct.unpack('!Q', stream.read(8))[0], 8
 
 Longlong = UnsignedLongLong
 
@@ -265,39 +265,36 @@ Longlong = UnsignedLongLong
 class Float(BaseType, float):
     TABLE_LABEL = b'f'
 
-    @classmethod
-    def pack(cls, value):
-        return struct.pack('!f', value)
+    def pack(self):
+        return struct.pack('!f', self)
 
     @classmethod
     def unpack(cls, stream: io.BytesIO):
-        return struct.unpack('!f', stream.read(4))
+        return struct.unpack('!f', stream.read(4))[0], 4
 
 
 class Double(BaseType, float):
     TABLE_LABEL = b'd'
 
-    @classmethod
-    def pack(cls, value):
-        return struct.pack('!d', value)
+    def pack(self):
+        return struct.pack('!d', self)
 
     @classmethod
     def unpack(cls, stream: io.BytesIO):
-        return struct.unpack('!d', stream.read(8))
+        return struct.unpack('!d', stream.read(8))[0], 8
 
 
 class Decimal(BaseType, decimal.Decimal):
     TABLE_LABEL = b'D'
 
-    @classmethod
-    def pack(cls, value):
-        sign, digits, exponent = value.as_tuple()
+    def pack(self):
+        sign, digits, exponent = self.as_tuple()
         v = 0
         for d in digits:
             v = v * 10 + d
         if sign:
             v = -v
-        return UnsignedByte.pack(-exponent) + UnsignedLong.pack(v)
+        return UnsignedByte(-exponent).pack() + UnsignedLong(v).pack()
 
     @classmethod
     def unpack(cls, stream: io.BytesIO):
@@ -306,45 +303,52 @@ class Decimal(BaseType, decimal.Decimal):
         total += consumed
         v, consumed = UnsignedLong.unpack(stream)
         total += consumed
-        return cls(v) / cls(10 ** exponent), consumed
+        return cls(v) / cls(10 ** exponent), total
 
 
 class ShortStr(BaseType, str):
     # Missing in rabbitmq/qpid
     TABLE_LABEL = None
+    MAX = UnsignedByte.MAX
 
-    def __new__(cls, value: str):
-        assert len(value) <= UnsignedByte.MAX
-        return super().__new__(cls, value)
-
-    @classmethod
-    def pack(cls, value):
+    def __new__(cls, value: str = ''):
         if isinstance(value, str):
             buffer = value.encode('utf-8')
         else:
             buffer = value
-        return UnsignedByte.pack(len(buffer)) + buffer
+        assert len(buffer) <= cls.MAX
+        instance = super().__new__(cls, value)
+        instance._buffer = buffer
+        return instance
+
+    def pack(self):
+        return UnsignedByte(len(self._buffer)).pack() + self._buffer
 
     @classmethod
     def unpack(cls, stream: io.BytesIO):
         str_len, consumed = UnsignedByte.unpack(stream)
         value = stream.read(str_len).decode('utf-8')
-        return value, consumed + str_len
+        return cls(value), consumed + str_len
 
 Shortstr = ShortStr
 
 
 class LongStr(BaseType, str):
     TABLE_LABEL = b'S'
+    MAX = UnsignedLong.MAX
 
-    def __new__(cls, value: str):
-        assert len(value) < UnsignedLong.MAX
-        return super().__new__(cls, value)
+    def __new__(cls, value: str = ''):
+        if isinstance(value, str):
+            buffer = value.encode('utf-8')
+        else:
+            buffer = value
+        assert len(buffer) <= cls.MAX
+        instance = super().__new__(cls, value)
+        instance._buffer = buffer
+        return instance
 
-    @classmethod
-    def pack(cls, value):
-        buffer = value.encode('utf-8')
-        return UnsignedLong.pack(len(buffer)) + buffer
+    def pack(self):
+        return UnsignedLong(len(self._buffer)).pack() + self._buffer
 
     @classmethod
     def unpack(cls, stream: io.BytesIO):
@@ -352,7 +356,7 @@ class LongStr(BaseType, str):
         value = stream.read(str_len)
         assert len(value) == str_len
         value = value.decode('utf-8')
-        return value, consumed + str_len
+        return cls(value), consumed + str_len
 
 Longstr = LongStr
 
@@ -360,12 +364,11 @@ Longstr = LongStr
 class Void(BaseType):
     TABLE_LABEL = b'V'
 
-    def __init__(self, value):
+    def __init__(self, value=None):
         pass
 
-    @classmethod
-    def pack(cls, value):
-        return b'V'
+    def pack(self):
+        return b''  # Nothing to return - it's Void!
 
     @classmethod
     def unpack(cls, stream: io.BytesIO):
@@ -378,9 +381,8 @@ class ByteArray(BaseType, bytes):
 
     TABLE_LABEL = b'x'
 
-    @classmethod
-    def pack(cls, value):
-        return UnsignedLong.pack(len(value)) + value
+    def pack(self):
+        return UnsignedLong(len(self)).pack() + self
 
     @classmethod
     def unpack(cls, stream: io.BytesIO):
@@ -393,7 +395,9 @@ class ByteArray(BaseType, bytes):
 class Timestamp(BaseType, datetime.datetime):
     TABLE_LABEL = b'T'
 
-    def __new__(cls, value: datetime.datetime):
+    def __new__(cls, value: datetime.datetime = None):
+        if value is None:
+            value = datetime.datetime.utcnow()
         return super().__new__(
             cls,
             value.year, value.month, value.day,
@@ -403,60 +407,60 @@ class Timestamp(BaseType, datetime.datetime):
     def __eq__(self, other):
         return abs(self - other) < datetime.timedelta(milliseconds=1)
 
-    @classmethod
-    def pack(cls, value):
-        stamp = int(value.timestamp())
-        return UnsignedLongLong.pack(stamp)
+    def pack(self):
+        stamp = int(self.timestamp())
+        return UnsignedLongLong(stamp).pack()
 
     @classmethod
     def unpack(cls, stream: io.BytesIO):
         value, consumed = UnsignedLongLong.unpack(stream)
-        return cls.fromtimestamp(value), consumed
+        date = datetime.datetime.fromtimestamp(value)
+        return cls(date), consumed
 
 
 class Table(BaseType, dict):
     TABLE_LABEL = b'F'
 
-    @classmethod
-    def pack(cls, value):
+    def pack(self):
         stream = io.BytesIO()
-        for key, value in value.items():
-            ShortStr(key).to_bytestream(stream)
+        for key, value in self.items():
+            key.to_bytestream(stream)
             stream.write(value.TABLE_LABEL)
             value.to_bytestream(stream)
         buffer = stream.getvalue()
-        return UnsignedLong.pack(len(buffer)) + buffer
+        return UnsignedLong(len(buffer)).pack() + buffer
 
     @classmethod
     def unpack(cls, stream: io.BytesIO):
         result = {}
-        table_len, consumed = UnsignedLong.unpack(stream)
-        initial = consumed
+        table_len, _ = UnsignedLong.unpack(stream)
+        consumed = 0
 
-        while consumed < table_len + initial:
+        while consumed < table_len:
             key, x = ShortStr.unpack(stream)
             consumed += x
 
             label = stream.read(1)
             consumed += 1
 
-            value, x = TABLE_LABEL_TO_CLS[label].unpack(stream)
+            amqptype = TABLE_LABEL_TO_CLS[label]
+            value, x = amqptype.unpack(stream)
             consumed += x
 
-            result[key] = value
+            result[key] = amqptype(value)
         return result, consumed
 
 
 class Array(BaseType, list):
     TABLE_LABEL = b'A'
 
-    @classmethod
-    def pack(cls, value):
+    def pack(self):
         stream = io.BytesIO()
-        for item in value:
-            item.to_bytestream(stream)
+        for value in self:
+            stream.write(value.TABLE_LABEL)
+            value.to_bytestream(stream)
         buffer = stream.getvalue()
-        return UnsignedLong.pack(len(buffer)) + buffer
+        return UnsignedLong(len(buffer)).pack() + buffer
 
     @classmethod
     def unpack(cls, stream: io.BytesIO):
@@ -464,14 +468,15 @@ class Array(BaseType, list):
         array_len, consumed = UnsignedLong.unpack(stream)
         initial = consumed
 
-        while consumed < array_len + initial:
+        while consumed < (array_len + initial):
             label = stream.read(1)
             consumed += 1
 
-            value, x = TABLE_LABEL_TO_CLS[label].unpack(stream)
+            amqptype = TABLE_LABEL_TO_CLS[label]
+            value, x = amqptype.unpack(stream)
             consumed += x
 
-            result.append(value)
+            result.append(amqptype(value))
         return result, consumed
 
 
